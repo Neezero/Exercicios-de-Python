c15d72d02826b51132e32e09720403e70f4b1d7b
Neezero <65928235+Neezero@users.noreply.github.com>
2020-08-23 21:23:02 -0300

    Delete Desafio2.py

--- Desafio2.py
+++
@@ -1,5 +0,0 @@
-
-dia = input('Qual dia você nasceu?')
-mês = input('Qual mês você nasceu?')
-ano = input('Que ano você nasceu?')
-print('Você nasceu no dia', dia, 'de', mês, 'do ano de', ano, 'certo?')
