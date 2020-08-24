fc22c2df68e65f7b06945d7f09319a263a8b7376
Neezero <65928235+Neezero@users.noreply.github.com>
2020-08-23 21:33:06 -0300

    Add files via upload

---
+++ Desafio2.py
@@ -0,0 +1,4 @@
+dia = input('Qual dia você nasceu?')
+mês = input('Qual mês você nasceu?')
+ano = input('Qual ano você nasceu?')
+print('Você nasceu em', dia, 'do mês de', mês, 'do ano de', ano, 'certo?')
